3977a9855f1ec9a2cb2ef71e9840b55d276fb56e
Gunaranjan Pemmaraju <gunaranjan.pemmaraju@gmail.com>
2014-08-10 15:44:37 +0530

    Plot2 Added

---
+++ plot2.R
@@ -0,0 +1,17 @@
+rm(list=ls())
+#variable setup
+c1<-rep("numeric",7)
+c2<-rep("character",2)
+co<-c(c2,c1)
+d1<-as.Date("2007-02-01")
+d2<-as.Date("2007-02-02")
+#reading data
+df<-read.table("household_power_consumption.txt", sep=";",na.strings="?",comment.char = "",header = TRUE,colClasses=co)
+df<-transform(df,dateTime=strptime(paste(df$Date, df$Time), format="%d/%m/%Y%H:%M:%S"), Date=as.Date(df$Date, format="%d/%m/%Y"))
+df<-df[df$Date>=d1 & df$Date<=d2,]
+#plotting
+plot(df$dateTime,df$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="n")
+lines(df$dateTime,df$Global_active_power)
+#File Save
+dev.copy(png, file = "plot2.png")
+dev.off()
